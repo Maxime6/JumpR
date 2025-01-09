f964e6643defb9df0996a1171d7a1b9ec747540a
Max6me <maxime.tanter@hotmail.fr>
2025-01-09 16:27:46 +0100

    Initial Commit

---
+++ JumpR/JumpRApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumpRApp.swift
+//  JumpR
+//
+//  Created by Maxime Tanter on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct JumpRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
