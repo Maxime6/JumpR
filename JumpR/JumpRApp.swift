ee6ef16ff9aa458bf0cd0bb08605dd9b337d44c5
Max6me <maxime.tanter@hotmail.fr>
2025-01-21 14:56:19 +0100

    Remove Firebase package to start the project with CloudKit and clean code

--- JumpR/JumpRApp.swift
+++ JumpR/JumpRApp.swift
@@ -6,19 +6,9 @@
 //
 
 import SwiftUI
-import Firebase
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
-        return true
-    }
-}
 
 @main
 struct JumpRApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
